5dfadc2064bd32835a658e7d5d425f4246f669a2
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2021-12-21 22:31:00 -0500

    commit 2
    En este punto el código carga preguntas aleatorias dependiendo de el nivel al que pertenezca el usuario

--- main.cpp
+++ main.cpp
@@ -3,70 +3,171 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    ifstream arch;
-    string question = "", level = "", option_a = "", option_b = "", option_c = "", option_d = "", answer = "", answer_player="";
+    ifstream arch; //inicializo archivo de preguntas
+    ifstream arch_points; //inicializo archivo de usuarios
+    string question = "", option_a = "", option_b = "", option_c = "", option_d = "", answer = "", answer_player="";
+    string player="", playerArch = "", name_arch = "";
+    int playerspoint=0, dec=0;
     vector <string> text;
-    string name_arch = "Questions.txt";
-    arch.open(name_arch);
+    cout<<".___________________________________________."<<endl;
+    cout<<"|             Menu de eleccion              |"<<endl;
+    cout<<".-------------------------------------------."<<endl;
+    cout<<"|Para ser el jugador 1 presione 1 (1)       |"<<endl;
+    cout<<"|Para ser el jugador 2 presione 2 (1)       |"<<endl;
+    cout<<"|Para ser el jugador 3 presione 3 (3)       |"<<endl;
+    cout<<"|Para salir presione (0)                    |"<<endl;
+    cout<<"---------------------------------------------"<<endl;
+    cin>>dec;
+    cin.ignore();
+
+    if(dec == 1)// se le asigna el jugador que el usuario elige
+        player="player1";
+    else if (dec==2)
+        player="player2";
+    else if (dec==3)
+        player="player3";
+
+    srand(time(NULL));//elijo un numero aleatorio, se usara para obtener pregunta aleatoria
+    int num1=rand()%5, num2=0;
+
+    cout<<"pregunta numero: "<<num1+1<<endl;
+
+    if (num1 == 0)
+        num1 = 0;
+    else if (num1 == 1)
+        num1=6;
+    else if (num1 == 2)
+        num1=12;
+    else if (num1 == 3)
+        num1=18;
+    else if (num1 == 4)
+        num1=24;
+
+    string name_points = "points.txt";
+
+    arch_points.open(name_points);
+
+    if(!arch_points){ // verifico que jugador es el usuario
+        cout<<"No fue posible abrir el archivo: "<<name_points<<endl;
+    }
+    else{
+        while(!arch_points.eof()){
+            arch_points>>playerArch;
+            arch_points>>playerspoint;
+            if (playerArch == player){
+                break;
+            }
+        }
+    }
+
+    if (playerspoint < 5) // se le informa al usuario en que nivel esta
+        cout<<"usted esta en el nivel 1";
+    else if (playerspoint >= 5 && playerspoint < 10)
+        cout<<"usted esta en el nivel 2";
+    else if (playerspoint >= 10 && playerspoint < 15)
+        cout<<"usted esta en el nivel 3";
+    else if (playerspoint >= 15 && playerspoint < 20)
+        cout<<"usted esta en el nivel 4";
+    else if (playerspoint > 19 )
+        cout<<"usted esta en el nivel 5";
+    cout<<endl<<endl;
+
+
+
+    if(playerspoint == 0){ // adquiero el jugador y su puntaje, y  se abre el archivo de preguntas correpondiente
+        name_arch = "level1.txt";
+        arch.open(name_arch);
+    }
+    else if(playerspoint == 5){
+        name_arch = "level2.txt";
+        arch.open(name_arch);
+    }
+    else if(playerspoint == 10){
+        name_arch = "level3.txt";
+        arch.open(name_arch);
+    }
+
     if(!arch){
         cout<<"No fue posible abrir el archivo: "<<name_arch<<endl;
     }
     else{
+        while(!arch.eof()){ // leo las preguntas del archivo y recibo una respuesta del jugador
+            if (num2 == num1){ //mecanismo para encontrar la pregunta aleatoria
+                getline(arch, question,'\n');
+                if(question == "end")
+                    break;
 
-        while(!arch.eof()){
-            getline(arch, level,'\n');
+                text.push_back(question);
 
-            if(level == "end")
-                break;
+                getline(arch, option_a,'\n');
+                text.push_back(option_a);
 
-            text.push_back(level);
+                getline(arch, option_b,'\n');
+                text.push_back(option_b);
 
-            getline(arch, question,'\n');
-            text.push_back(question);
+                getline(arch, option_c,'\n');
+                text.push_back(option_c);
 
-            getline(arch, option_a,'\n');
-            text.push_back(option_a);
+                getline(arch, option_d,'\n');
+                text.push_back(option_d);
 
-            getline(arch, option_b,'\n');
-            text.push_back(option_b);
+                getline(arch, answer,'\n');
+                //text.push_back(answer);
 
-            getline(arch, option_c,'\n');
-            text.push_back(option_c);
+                for (unsigned int i = 0; i < text.size() ; i++){
+                    cout<<text[i]<<endl;
+                }
 
-            getline(arch, option_d,'\n');
-            text.push_back(option_d);
-
-            getline(arch, answer,'\n');
-            text.push_back(answer);
-
-            cout<<"usted esta en el "<<text[0]<<endl;
-            for (unsigned int i = 1; i < text.size() ; i++){
-                cout<<text[i]<<endl;
-            }
-
-            cout<<"seleccione una opcion: ";
-            cin>>answer_player;
-            cin.ignore();
+                cout<<"seleccione una opcion: ";
+                cin>>answer_player;
+                cin.ignore();
 
 
-
-
-            if (answer == answer_player){
-                cout<<"¡Acertaste la respuesta!, FELICIDADES."<<endl;
+                if (answer == answer_player){
+                    system("cls");
+                    cout<<"Acertaste la respuesta!, FELICIDADES."<<endl;
+                }
+                else{
+                    system("cls");
+                    cout<<"Respuesta incorrecta"<<endl;
+                }
+                text.clear();
             }
             else{
-                cout<<"Respuesta incorrecta"<<endl;
+                getline(arch, question,'\n');
+                if(question == "end")
+                    break;
+
+                //text.push_back(question);
+
+                getline(arch, option_a,'\n');
+                //text.push_back(option_a);
+
+                getline(arch, option_b,'\n');
+                //text.push_back(option_b);
+
+                getline(arch, option_c,'\n');
+                //text.push_back(option_c);
+
+                getline(arch, option_d,'\n');
+                //text.push_back(option_d);
+
+                getline(arch, answer,'\n');
+
             }
-            text.clear();
+            num2+=6;
+
         }
     }
 
-
+    arch_points.close();
     arch.close();
     return 0;
 }
